daa0bd3994ec3b5748d474a88f07800c93e38ffc
tjh <qinaqina552188@163.com>
2020-07-19 12:19:03 +0800

    1

--- dataservice/src/main/java/com/tjhnode/dataservice/controller/SysPermissionController.java
+++ dataservice/src/main/java/com/tjhnode/dataservice/controller/SysPermissionController.java
@@ -14,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
